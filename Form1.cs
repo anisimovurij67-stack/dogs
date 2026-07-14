cfb52ae9c2210f50fd66e3e4dcc71d48780ad126
анисимов юрий <anisimovurij67@gmail.com>
2026-07-15 02:50:20 +0300

    НН

--- Form1.cs
+++ Form1.cs
@@ -12,30 +12,19 @@ namespace DogPep
         public Form1()
         {
             InitializeComponent();
-
-            // ВАЖНО: Ручная привязка. 
-            // Если твои кнопки на форме называются как-то иначе (не button1 и button2),
-            // просто поменяй названия ниже на те, что у тебя в конструкторе.
-
             button1.Click += BtnImage_Click;
             btnGetFact.Click += BtnFact_Click;
         }
-
-        // Логика для картинки
         private async void BtnImage_Click(object sender, EventArgs e)
         {
             string url = await api.GetDogImage();
             if (url != "")
             {
-                // Замени pictureBox1 на свое название, если нужно
                 pictureBox1.Load(url);
             }
         }
-
-        // Логика для текста
         private async void BtnFact_Click(object sender, EventArgs e)
         {
-            // Замени textBox1 на свое название, если нужно
             txtFact.Text = "Ищем собаку...";
             string fact = await api.GetDogFact();
             txtFact.Text = fact;
